d7c600550dfd5e9e3354b731c169c2b9536e6c78
GDuath <duath83@gmx.de>
2023-06-13 19:31:00 +0200

    Added additional clan fields

--- TerritorialHQ Class Libraries/Entities/Clan.cs
+++ TerritorialHQ Class Libraries/Entities/Clan.cs
@@ -19,6 +19,14 @@ public class Clan : IEntity
     [Required]
     [Display(Name = "Discord Guild ID")]
     public ulong? GuildId { get; set; }
+    [Display(Name = "Date / Period of Foundation")]
+    public string? Foundation { get; set; }
+    [Display(Name = "Founder(s)")]
+    public string? Founders { get; set; }
+    [Display(Name = "Clan Motto")]
+    public string? Motto { get; set; }
+    [Display(Name = "Custom Bot HttpGet Endpoint")]
+    public string? BotEndpoint { get; set; }
     [Display(Name = "Logo file")]
     public string? LogoFile { get; set; }
     [Display(Name = "Banner file")]
