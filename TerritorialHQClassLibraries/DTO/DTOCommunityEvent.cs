41e9dd473045f52c4258a91971de8f2eb6b2a148
GDuath <duath83@gmx.de>
2023-08-10 23:55:27 +0200

    Added community event classes

---
+++ TerritorialHQClassLibraries/DTO/DTOCommunityEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerritorialHQ_Library.DTO.Interface;
+
+namespace TerritorialHQ_Library.DTO
+{
+    public class DTOCommunityEvent : IDto
+    {
+        public string? Id { get; set; }
+        public string? Creator { get; set; }
+
+        [Display(Name = "Published")]
+        public bool IsPublished { get; set; }
+        [Display(Name = "In Review")]
+        public bool InReview { get; set; }
+
+        [Display(Name = "Title")]
+        public string? Title { get; set; }
+        [Display(Name = "Event Description")]
+        public string? Description { get; set; }
+
+        [Display(Name = "Start (UTC)")]
+        public DateTime? Start { get; set; }
+        [Display(Name = "End (UTC)")]
+        public DateTime? End { get; set; }
+        [Display(Name = "Recurring Event")]
+        public bool Recurring { get; set; }
+        [Display(Name = "Interval (every X days)")]
+        public int? Interval { get; set; }
+
+        [Display(Name = "Event location (server / channel)")]
+        public string? Location { get; set; }
+        [Display(Name = "Discord server invite link")]
+        public string? DiscordServerLink { get; set; }
+        [Display(Name = "Host")]
+        public string? Host { get; set; }
+        [Display(Name = "Image file")]
+        public string? ImageFile { get; set; }
+    }
+}
---
+++ TerritorialHQClassLibraries/Entities/CommunityEvent.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerritorialHQ_Library.DTO;
+using TerritorialHQ_Library.DTO.Interface;
+
+namespace TerritorialHQ_Library.Entities
+{
+    public class CommunityEvent : IEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string? Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string? Creator { get; set; }
+
+        public bool IsPublished { get; set; }
+        public bool InReview { get; set; }
+
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+        public bool Recurring { get; set; }
+        public int? Interval { get; set; }
+
+        public string? Location { get; set; }
+        public string? DiscordServerLink { get; set; }
+        public string? Host { get; set; }
+        public string? ImageFile { get; set; }
+
+        public IDto GetDto()
+        {
+            return new DTOCommunityEvent()
+            {
+                Id = Id,
+                Creator = Creator,
+                Title = Title,
+                Description = Description,
+                Start = Start,
+                End = End,
+                Recurring = Recurring,
+                Interval = Interval,
+                Location = Location,
+                DiscordServerLink = DiscordServerLink,
+                Host = Host,
+                ImageFile = ImageFile,
+                IsPublished = IsPublished,
+                InReview = InReview
+            };
+        }
+
+        public void MapDto(IDto dto)
+        {
+            var evt = (DTOCommunityEvent)dto;
+
+            this.Title = evt.Title;
+            this.Description = evt.Description;
+            this.Start = evt.Start;
+            this.End = evt.End;
+            this.Recurring = evt.Recurring;
+            this.Interval = evt.Interval;
+            this.Location = evt.Location;
+            this.DiscordServerLink = evt.DiscordServerLink;
+            this.Host = evt.Host;
+            this.ImageFile = evt.ImageFile; 
+            this.IsPublished = evt.IsPublished;
+            this. InReview = evt.InReview;
+        }
+    }
+}
